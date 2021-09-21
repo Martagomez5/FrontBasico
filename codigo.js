5bf208312cb9a3ac6e523cdb3573494a39d9d2b8
Marta Gómez Ruiz <76521905+marta052000@users.noreply.github.com>
2021-09-21 14:26:09 +0200

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -2,3 +2,9 @@ function saludar(){
     window.alert("Has pulsado un boton"); /*objeto.metodo*/
     console.log("boton ok");
 }
+
+function numeros(){
+    for(var i =0; i<5; i++){
+       console.log(i);
+    };
+}
